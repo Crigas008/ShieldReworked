c635a11f0014a63f0cfd7e80686b8b9d255cb477
crigas <zxcrigas008@gmail.com>
2025-05-10 13:54:46 +0500

    Initial commit

---
+++ src/main/java/me/crigas/shieldrework/Shieldrework.java
@@ -0,0 +1,17 @@
+package me.crigas.shieldrework;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Shieldrework extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
